4684777995859204c3b7f6c01fcb2bf12e920084
kszabi1 <46687382+kszabi1@users.noreply.github.com>
2024-04-15 11:46:31 +0200

    project setup

--- src/main/java/com/codecool/askmateoop/dao/QuestionsDaoJdbc.java
+++ src/main/java/com/codecool/askmateoop/dao/QuestionsDaoJdbc.java
@@ -1,10 +1,16 @@
 package com.codecool.askmateoop.dao;
 
 import com.codecool.askmateoop.dao.model.Question;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public class QuestionsDaoJdbc implements QuestionsDAO {
+
+    //    TODO: Add the url of your database to the Environment Variables of the Run Configuration
+    //    @Value("${askmate.database.url}")
+    //    private String databaseUrl;
 
     @Override
     public List<Question> getAllQuestions() {
