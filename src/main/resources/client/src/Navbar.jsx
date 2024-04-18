a04694aeb3a0b1088d842a3687aea69896104d0a
kszabi1 <46687382+kszabi1@users.noreply.github.com>
2024-04-18 09:08:22 +0200

    Refactor the frontend

--- src/main/resources/client/src/Navbar.jsx
+++ src/main/resources/client/src/Navbar.jsx
@@ -1,16 +1,44 @@
-import React from 'react'
+import React, {useRef, useState} from 'react';
+import { Link } from 'react-router-dom';
+import LoginPopup from './components/LoginPopup';
 import "./css/Navbar.css"
 
 const Navbar = () => {
-  return (
-    <div className='navbar'>
-      <div className='navbarTitle'>
-        <h3 className='pageTitle'>QoverFlow</h3>
-      </div>
-        <p className='navbarText'>Home</p>
-        <p className='navbarText'>Questions</p>
-    </div>
-  )
+    const [showPopup, setShowPopup] = useState(false);
+    const popupRef = useRef(null);
+
+    const handleLoginClick = () => {
+        setShowPopup(true);
+    };
+
+    const handleClosePopup = () => {
+        setShowPopup(false);
+    };
+
+    const handleClickOutside = (e) => {
+        if (popupRef.current && !popupRef.current.contains(e.target)) {
+            handleClosePopup();
+        }
+    };
+
+    React.useEffect(() => {
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+
+    return (
+        <div className='navbar'>
+            <div className='navbarTitle'>
+                <h3 className='pageTitle'>QoverFlow</h3>
+            </div>
+            <Link to="/" className="navbarText">Home</Link>
+            <Link to="/questions" className="navbarText">Questions</Link>
+            <button onClick={handleLoginClick} className="navbarText">Login</button>
+            {showPopup && <LoginPopup ref={popupRef} onClose={handleClosePopup} />}
+        </div>
+    )
 }
 
 export default Navbar;
